aa45bf7f48ac4fc5119baa5365a749bfa4164410
Maarten van der Aart <maarten@buttonbasher.org>
2016-12-08 17:46:23 +0100

    Add some randomness

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,20 @@
  * Created by maarten on 08/12/2016.
  */
 import static spark.Spark.*;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+
+
 
 public class Main {
     public static void main(String[] args) {
         port(5000);
         get("/", (req, res) -> "Hello Cloudy \n");
+        get("/act-randomly", (req, res) -> {
+            Document doc = Jsoup.connect("http://www.setgetgo.com/randomword/get.php").get();
+            String text = doc.body().text() + "\n";
+            return text;
+        });
+
     }
 }
